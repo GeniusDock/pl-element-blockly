b9eaece77c05a73bdcbd9d070957c12b25872cc9
feche <fecheromero@gmail.com>
2018-11-10 15:47:47 -0300

    fix capitalize + clean code

--- js/block-aliases.js
+++ js/block-aliases.js
@@ -1,37 +1,10 @@
 Blockly.PrologLanguage.blockIDAliases = {
-  "ComandosPrimitivos": "Comandos primitivos",
-  "ProcedimientosPrimitivos": "Procedimientos primitivos",
-  "MisProcedimientos": "Mis procedimientos",
-  "ExpresionesPrimitivas": "Expresiones primitivas",
-  "FuncionesPrimitivas": "Funciones primitivas",
   "CategoriaAsignacion":"Asignación",
   "Color":"ColorSelector",
   "Individuo":"IndividuoSelector",
   "Variable":"VariableSelector",
-  "Direccion":"DireccionSelector",
-  "Numero":"math_number",
-  "Booleano":"BoolSelector",
-  "ExpresionesPrimitivas": "Expresiones primitivas",
-  "FuncionesPrimitivas": "Funciones primitivas",
-  "MisFunciones": "Mis funciones",
   "OperadorDeNegacion": "not",
-  "OperadorSiguiente": "siguiente",
-  "OperadorPrevio": "previo",
-  "OperadorOpuesto": "opuesto",
-  "DefinicionesDeProgramas": "Programas",
-  "DefinicionDeProgramaComun": "Program",
-  "DefinicionDeProgramaInteractivo": "InteractiveProgram",
-  "DefinicionDeAsociacionDeTeclaLetra": "InteractiveLetterBinding",
-  "DefinicionDeAsociacionDeTeclaNumero": "InteractiveNumberBinding",
-  "DefinicionDeAsociacionDeTeclaEspecial": "InteractiveKeyBinding",
-  "DefinicionesDeProcedimientos": "Procedimientos",
-  "DefinicionDeProcedimientoSimple": "procedures_defnoreturnnoparams",
-  "DefinicionDeProcedimientoParametrizado": "procedures_defnoreturn",
-  "DefinicionesDeFunciones": "Funciones",
-  "DefinicionDeFuncionSimple": "procedures_defreturnsimple",
-  "DefinicionDeFuncionSimpleParametrizada": "procedures_defreturnsimplewithparams",
-  "DefinicionDeFuncionParametrizada": "procedures_defreturn"
-};
+ };
 
 Blockly.PrologLanguage.aliasForBlockID = function(id){
   return  this.blockIDAliases[id] || id;
